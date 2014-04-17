467913d40c5d9fe73c5b81af93f3f840153c3d68
victor <victor@victor-VirtualBox.(none)>
2014-04-17 16:22:52 -0700

    new implementation backward search

--- simple_taboo_search/simple_taboo_search-6.c
+++ simple_taboo_search/simple_taboo_search-6.c
@@ -27,22 +27,53 @@
  *
  * prints in the right format for the read routine
  */
+
+int maximumGraphSize = 101;
+
+int edgeWeight [101];
+
+void initialize_edge_weight()
+{
+	int i =0;
+	for (i;i<maximumGraphSize;i++)
+	{
+		edgeWeight[i] = 0;
+	}
+}
+
+int get_max_edge_index ()
+{
+	int maximum_index = 0;
+	int maximum_weight = edgeWeight[maximum_index];
+	int i =1;
+	for (i;i<maximumGraphSize;i++)
+	{
+		if(edgeWeight[i]>maximum_weight)
+		{
+			maximum_index = i;
+			maximum_weight = edgeWeight[i];
+		}
+	}
+	return maximum_index;
+}
+
+
+
 void PrintGraph(int *g, int gsize)
 {
 	int i;
 	int j;
 
-	fprintf(stdout,"%d\n",gsize);
-
+	FILE * pFile = fopen ("myfileAll.txt","a");
 	for(i=0; i < gsize; i++)
 	{
 		for(j=0; j < gsize; j++)
 		{
-			fprintf(stdout,"%d ",g[i*gsize+j]);
+			fprintf(pFile,"%d",g[i*gsize+j]);
 		}
-		fprintf(stdout,"\n");
 	}
-
+	fprintf(pFile, "\n");
+	fclose(pFile);
 	return;
 }
 
@@ -97,7 +128,7 @@ int CliqueCount(int *g,
     int n;
     int count=0;
     int sgsize = 6;
-    
+    initialize_edge_weight();
     for(i=0;i < gsize-sgsize+1; i++)
     {
 	for(j=i+1;j < gsize-sgsize+2; j++)
@@ -113,7 +144,8 @@ int CliqueCount(int *g,
 			   (g[i*gsize+j] == g[j*gsize+l]) && 
 			   (g[i*gsize+j] == g[k*gsize+l]))
 			{
-			    for(m=l+1;m < gsize-sgsize+5; m++) 
+			    //for(m=l+1;m < gsize-sgsize+5; m++) 
+			    m = gsize-1;
 			    {
 				if((g[i*gsize+j] == g[i*gsize+m]) && 
 				   (g[i*gsize+j] == g[j*gsize+m]) &&
@@ -121,7 +153,8 @@ int CliqueCount(int *g,
 				   (g[i*gsize+j] == g[l*gsize+m])) {
 					for(n=m+1; n < gsize-sgsize+6; n++)
 					{
-						if((g[i*gsize+j]
+						if((g[i*
+							++j]
 							== g[i*gsize+n]) &&
 						   (g[i*gsize+j] 
 							== g[j*gsize+n]) &&
@@ -160,158 +193,95 @@ main(int argc,char *argv[])
 	int best_j;
 	void *taboo_list;
 
-	/*
-	 * start with graph of size 8
-	 */
-	gsize = 8;
-	g = (int *)malloc(gsize*gsize*sizeof(int));
-	if(g == NULL) {
-		exit(1);
-	}
-
-	/*
-	 * make a fifo to use as the taboo list
-	 */
-	taboo_list = FIFOInitEdge(TABOOSIZE);
-	if(taboo_list == NULL) {
-		exit(1);
-	}
-
-	/*
-	 * start out with all zeros
-	 */
-	memset(g,0,gsize*gsize*sizeof(int));
-
-	/*
-	 * while we do not have a publishable result
-	 */
-	while(gsize < 102)
-	{
-		/*
-		 * find out how we are doing
-		 */
-		count = CliqueCount(g,gsize);
-
-		/*
-		 * if we have a counter example
-		 */
-		if(count == 0)
-		{
-			printf("Eureka!  Counter-example found!\n");
-			PrintGraph(g,gsize);
-			/*
-			 * make a new graph one size bigger
-			 */
-			new_g = (int *)malloc((gsize+1)*(gsize+1)*sizeof(int));
-			if(new_g == NULL)
+	// read counter example from file
+   FILE * pFile;
+   char graph_size [10];
+   char mystring [10202];
+   pFile = fopen ("example.txt" , "r");
+   if (pFile == NULL) perror ("Error opening file");
+   else {
+     if(fgets(graph_size,10,pFile)!=NULL)
+     {
+     		gsize = atoi(graph_size);
+     		printf("%d\n", gsize);
+     		g = (int *)malloc(gsize*gsize*sizeof(int));
+			if(g == NULL) {
 				exit(1);
-			/*
-			 * copy the old graph into the new graph leaving the
-			 * last row and last column alone
-			 */
-			CopyGraph(g,gsize,new_g,gsize+1);
-
-			/*
-			 * zero out the last column and last row
-			 */
-			for(i=0; i < (gsize+1); i++)
-			{
-				new_g[i*(gsize+1) + gsize] = 0; // last column
-				new_g[gsize*(gsize+1) + i] = 0; // last row
 			}
-
-			/*
-			 * throw away the old graph and make new one the
-			 * graph
-			 */
-			free(g);
-			g = new_g;
-			gsize = gsize+1;
-
-			/*
-			 * reset the taboo list for the new graph
-			 */
-			taboo_list = FIFOResetEdge(taboo_list);
-
-			/*
-			 * keep going
-			 */
-			continue;
-		}
-
-		/*
-		 * otherwise, we need to consider flipping an edge
-		 *
-		 * let's speculative flip each edge, record the new count,
-		 * and unflip the edge.  We'll then remember the best flip and
-		 * keep it next time around
-		 *
-		 * only need to work with upper triangle of matrix =>
-		 * notice the indices
-		 */
-		best_count = BIGCOUNT;
-		for(i=0; i < gsize; i++)
+     }
+     if ( fgets (mystring , gsize*gsize , pFile) != NULL )
+     {
+     	int element_count = 0;
+     	for (element_count;element_count<gsize*gsize;element_count++)
 		{
-			for(j=i+1; j < gsize; j++)
-			{
-				/*
-				 * flip it
-				 */
-				g[i*gsize+j] = 1 - g[i*gsize+j];
-				count = CliqueCount(g,gsize);
-
-				/*
-				 * is it better and the i,j,count not taboo?
-				 */
-				if((count < best_count) && 
-					!FIFOFindEdge(taboo_list,i,j))
-//					!FIFOFindEdgeCount(taboo_list,i,j,count))
+				if(mystring[element_count]=='0')
 				{
-					best_count = count;
-					best_i = i;
-					best_j = j;
+					g[element_count] = 0;
+				}
+				else
+				{
+					g[element_count] = 1;
 				}
 
-				/*
-				 * flip it back
-				 */
-				g[i*gsize+j] = 1 - g[i*gsize+j];
-			}
 		}
+     }
+     
+     fclose (pFile);
+   }
+	
+   generate_counter_example(g,gsize);
+   return(0);
 
-		if(best_count == BIGCOUNT) {
-			printf("no best edge found, terminating\n");
-			exit(1);
-		}
-		
-		/*
-		 * keep the best flip we saw
-		 */
-		g[best_i*gsize+best_j] = 1 - g[best_i*gsize+best_j];
+}
 
-		/*
-		 * taboo this graph configuration so that we don't visit
-		 * it again
-		 */
-		count = CliqueCount(g,gsize);
-		FIFOInsertEdge(taboo_list,best_i,best_j);
-//		FIFOInsertEdgeCount(taboo_list,best_i,best_j,count);
 
-		printf("ce size: %d, best_count: %d, best edge: (%d,%d), new color: %d\n",
-			gsize,
-			best_count,
-			best_i,
-			best_j,
-			g[best_i*gsize+best_j]);
-
-		/*
-		 * rinse and repeat
-		 */
+// generate counter examples of size n-1
+void generate_counter_example(int * g, int gsize)
+{
+	if(gsize<=79)
+	{
+		return;
 	}
+	else
+	{
 
-	FIFODeleteGraph(taboo_list);
+		int count = CliqueCount(g,gsize);
 
-
-	return(0);
+		if(count == 0)
+		{
+		   PrintGraph(g,gsize);
+		   //get all counter examples of size n-1
+		   // current_node to be deleted from the original graph
+		   int current_node = 0;
+		   for(current_node = 0; current_node<gsize;current_node ++)
+		   {
+		   		int *  new_graph = (int *)malloc((gsize-1)*(gsize-1)*sizeof(int));
+				if(new_graph == NULL) {
+					exit(1);
+				}
+				int edge_counter = 0;
+				int row = 0;
+				int column = 0;
+				for(row;row<gsize;row++)
+				{
+					for(column=0;column<gsize;column++)
+					{
+						if(row==current_node||column==current_node)
+						{
+							continue;
+						}
+						new_graph[edge_counter++] = g[row*gsize+column];
+					}
+				}
+				count = CliqueCount(new_graph,gsize-1);
+				if(count == 0)
+				{
+				   PrintGraph(new_graph,gsize-1);
+				}
+				generate_counter_example(new_graph,gsize-1);
+				if(new_graph!=NULL)free(new_graph);
+		   }	
+		}
+	}
 
 }
