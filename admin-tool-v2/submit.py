b8cb269c98789859f1208bc3f5f8b4180832c5e1
Dibyendu Nath <dev.nath.cs@gmail.com>
2014-04-19 02:02:04 -0700

    Update submit.py

--- admin-tool-v2/submit.py
+++ admin-tool-v2/submit.py
@@ -4,6 +4,7 @@ import os
 import mod_vault
 import time
 import yaml
+from datetime import datetime
 
 filename = 'solution.txt'
 if len(sys.argv) == 2:        
@@ -18,13 +19,15 @@ settings_file = open(settings_file_name, 'r')
 settings = yaml.load(settings_file)
 settings_file.close()
 
-# skip = 1000
+### skipped 0
+skip = 0
+print 'skip =', skip
 count = -1
 for line in f:
 	count += 1
-	print 'count =', count
-	# if count < skip:
-	#	continue
+	print str(datetime.now()), 'count =', count
+	if count < skip:
+		continue
 	fw_name = filename + '_sol.txt'
 	fw = open(fw_name, 'w')
 	fw.write(line)
@@ -33,7 +36,7 @@ for line in f:
 	mod_vault_handler = mod_vault.get_module_handler(settings)
 	ret = mod_vault_handler.handle_command(['mint', fw_name])
 	while ret is not True:
-		print 'Attempt failed !'
+		print str(datetime.now()), 'Attempt failed ! count =', count
 		ret = mod_vault_handler.handle_command(['mint', fw_name])
 		time.sleep(3)		
 	time.sleep(3)
