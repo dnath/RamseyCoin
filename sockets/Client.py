f28d35f55bf8afd46ed5f576860d7c813d4134f0
Daniel Kudrow <dkudrow@cs.ucsb.edu>
2014-05-24 15:14:51 -0700

    Client is almost working

--- sockets/Client.py
+++ sockets/Client.py
@@ -2,85 +2,86 @@
 
 import socket               # Import socket module
 from json_formatter import *# Import json to object translation for counterexample class and message class 
+from Tabu import tabu
 
 client_id = 2
 server_ip  = "127.0.0.1"
 server_port = 12345                # Reserve a port for your service.
 client_port = 12500
-client_ip = socket.gethostname()
+client_hostname = "localhost"
 
 counter = 0 
 
 
 
 def get_seed():
-	request_message = message(0,"",client_id,client_ip,client_port)
-	s = socket.socket()         # Create a socket object
+    request_message = message(0,"",client_id,client_hostname,client_port)
+    s = socket.socket()         # Create a socket object
 
-	#Get server hostname, it returns an array with the hostname in the first element
-	host = socket.gethostbyaddr(server_ip)[0] 
-	#Connect to the server
-	s.connect((host, server_port))
-	#send getseed request to the server
-	s.send(request_message.get_json())
-	#receive the seed
-	seed = s.recv(15000).strip()
-	s.close()
-	response_message = message(seed)
-	return response_message.data.strip()
+    #Get server hostname, it returns an array with the hostname in the first element
+    server_host = socket.gethostbyaddr(server_ip)[0] 
+    #Connect to the server
+    print "Connecting to Server at %s:%d" % (server_host, server_port)
+    s.connect((server_host, server_port))
+    #send getseed request to the server
+    s.send(request_message.get_json())
+    #receive the seed
+    seed = s.recv(15000).strip()
+    s.close()
+    response_message = message(seed)
+    return response_message.data.strip()
 
 
 def save_seed(seed):
-	request_message = message(1,seed,client_id,client_ip,client_port)
-	s = socket.socket()         # Create a socket object
-	#Get server hostname, it returns an array with the hostname in the first element
-	host = socket.gethostbyaddr(server_ip)[0] 
-	#Connect to the server
-	s.connect((host, server_port))
-	#send getseed request to the server
-	s.send(request_message.get_json())
-	s.close()
+    request_message = message(1,seed,client_id,client_hostname,client_port)
+    s = socket.socket()         # Create a socket object
+    #Get server hostname, it returns an array with the hostname in the first element
+    server_host = socket.gethostbyaddr(server_ip)[0] 
+    #Connect to the server
+    s.connect((server_host, server_port))
+    #send getseed request to the server
+    s.send(request_message.get_json())
+    s.close()
 
 def bind_socket(host,port):
-	s = socket.socket()         # Create a socket object
-	s.bind((host, port))        # Bind to the port
-	s.listen(5)                 # Now wait for client connection.
+    s = socket.socket()         # Create a socket object
+    s.bind((host, port))        # Bind to the port
+    s.listen(5)                 # Now wait for client connection.
 
 
 #handle requests from the server
-def handle_request(request_json)
-	response_message = message(request_json)
-
-
-
+def handle_request(request_json):
+    response_message = message(request_json)
 
 def accept_connections():
-	while True:
-		c, addr = s.accept()     # Establish connection with client.
-		message_json = c.recv(15000)
-		try:
-		   	thread.start_new_thread( handle_request, (message_json))
-		except:
-	   		print "Error: unable to start thread"
+    while True:
+        c, addr = s.accept()     # Establish connection with client.
+        message_json = c.recv(15000)
+        try:
+            thread.start_new_thread(handle_request, (message_json))
+        except:
+            print "Error: unable to start thread"
 
+def vector_to_matrix(vector):
+    size = len(vector)
+    matrix_size = math.sqrt(size)
+    Matrix = [[0 for x in range(size)] for y in range(size)] 
+    for i in range(size):
+        for j in range(size):
+            Matrix[i][j] = vector[i*size+j]
 
-def vector_to_matrix(vector)
-	size = len(vector)
-	matrix_size = math.sqrt(size)
-	Matrix = [[0 for x in range(size)] for y in range(size)] 
-	for i in range(size):
-		for j in range(size):
-			Matrix[i][j] = vector[i*size+j]
-
-	return Matrix
-
+    return Matrix
 
 def main():
-	seed  = get_seed()
-	#start taboo search thread
-	thread.start_new_thread(taboo(seed, 1))
-	#bind a socket in the client for furthet communication
- 	bind_socket(client_ip,client_port)
- 	#listen for connections from the server
- 	accept_connections()
+    print "Starting RamseyCoin Client..."
+    print "> listening on %s:%d" % (client_hostname, client_port)
+    seed = get_seed()
+    print "Got seed, forking tabu search on separate thread"
+    #start taboo search thread
+    thread.start_new_thread(tabu(seed, 1))
+    #bind a socket in the client for furthet communication
+    bind_socket(client_hostname,client_port)
+    #listen for connections from the server
+    accept_connections()
 
+main()
--- sockets/Server.py
+++ sockets/Server.py
@@ -6,149 +6,143 @@ from os import listdir
 from os.path import isfile, join
 import math
 
-
-
-
 #global variables
 Id = 1
 clients_file = "clients.txt"
 solution_directory = "solutions/" #define solution directory where there is a file for every counterexample size.
 solution_prefix = "sol_"
 IP = "127.0.0.1"
-host_name = socket.gethostname() # Get local machine name
-port_number = 12345                # Reserve a port for your service.
+#server_hostname = socket.gethostname() # Get local machine name
+server_hostname = "localhost"
+server_port = 12345                # Reserve a port for your service.
 
 # Dictionary of client node which are running taboo search.
 # This dictionary should be saved on some presistent storage S3
 # It will be used in case of schedular failer
 client_dictionary = {}
 
-
-
-
-
+#Any node in the system has and Id, IP and port. 
+class Node: 
+    def __init__(self, Id, IP, Port):
+        self.Id = Id
+        self.IP = IP
+        self.Port = Port
 
 def list_file(directory):
-	onlyfiles = [ f for f in listdir(directory) if isfile(join(directory,f)) ]
-	return onlyfiles
+    onlyfiles = [ f for f in listdir(directory) if isfile(join(directory,f)) ]
+    return onlyfiles
 
 def handle_request(c, message_json):
-	request_message = message(message_json)
-	type = request_message.type
+    request_message = message(message_json)
+    type = request_message.type
 
-	#get_seed, should be the first message from the client when it joins the system
-	if type == 0: #get_seed
-		#add client to client list
-		clientNode = Node(request_message.Id,request_message.IP,request_message.Port)
-		client_dictionary[clientNode.Id] = clientNode
-		#save clients list
-		save_clients_file (clients_file)
-		#list solution files
-		file_list = list_file(solution_directory)
-		file_list.sort()
-		if(len(file_list)!=0):
-			f = open(solution_directory + file_list[len(file_list)-1],"r")
-			line = f.readline()
-			f.close()
-			response_message = message (2,line)
-			c.send(response_message.get_json())
-		else:
-			c.send("no counterexample available")
-   		c.close()
-	elif type == 1: #save_counter_example
-		data = request_message.data
-		size = math.sqrt(len(data))
-		filename = solution_directory + solution_prefix + str(size)
-		file_list = list_file(solution_directory)
-		f = open(filename, 'a')
-		f.write(data + "\n")
-		f.close()
-		c.close()
-		if filename not in file_list:
-			#new solution size
-			#broadcast this solution to every one
-			bcmessage = new message(2,data,Id,IP,port_number)
-			broad_cast(bcmessage)
-	#elif type == 2: # Heartbeat acknowledgement 
+    #get_seed, should be the first message from the client when it joins the system
+    if type == 0: #get_seed
+        print "Recieved 'get_seed' request from client"
+        #add client to client list
+        clientNode = Node(request_message.Id,request_message.IP,request_message.Port)
+        client_dictionary[clientNode.Id] = clientNode
+        #save clients list
+        save_clients_file (clients_file)
+        #list solution files
+        file_list = list_file(solution_directory)
+        file_list.sort()
+        if(len(file_list)!=0):
+            f = open(solution_directory + file_list[len(file_list)-1],"r")
+            line = f.readline()
+            f.close()
+            response_message = message (2,line)
+            c.send(response_message.get_json())
+        else:
+            c.send("no counterexample available")
+        c.close()
+    elif type == 1: #save_counter_example
+        print "Recieved 'save_counterexample' request from client"
+        data = request_message.data
+        size = math.sqrt(len(data))
+        filename = solution_directory + solution_prefix + str(size)
+        file_list = list_file(solution_directory)
+        f = open(filename, 'a')
+        f.write(data + "\n")
+        f.close()
+        c.close()
+        if filename not in file_list:
+            #new solution size
+            #broadcast this solution to every one
+            bcmessage = message(2,data,Id,IP,server_port)
+            broad_cast(bcmessage)
+    #elif type == 2: # Heartbeat acknowledgement 
 
 
 
 def bind_socket(host,port):
-	s = socket.socket()         # Create a socket object
-	s.bind((host, port))        # Bind to the port
-	s.listen(5)                 # Now wait for client connection.
+    s = socket.socket()         # Create a socket object
+    s.bind((host, port))        # Bind to the port
+    s.listen(5)                 # Now wait for client connection.
+    return s
 
-def accept_connections():
-	while True:
-		c, addr = s.accept()     # Establish connection with client.
-		message_json = c.recv(15000)
-		print(message_json)
-		try:
-		   	thread.start_new_thread( handle_request, (c, message_json))
-		except:
-	   		print "Error: unable to start thread"
+def accept_connections(s):
+    while True:
+        c, addr = s.accept()     # Establish connection with client.
+        message_json = c.recv(15000)
+        print(message_json)
+        try:
+            thread.start_new_thread( handle_request, (c, message_json))
+        except:
+            print "Error: unable to start thread"
 
-
-#Any node in the system has and Id, IP and port. 
-class Node: 
-	def __init__(self, Id, IP, Port):
-		self.Id = Id
-		self.IP = IP
-		self.Port = Port
 
 
 # Read clients' connection data from file to a dictionary
 def read_clients_file(file_name):
-	fp = open(file_name,"r")
-	#line in the form, clientID, IP and Port
-	clientStrings = fp.readlines()
-	fp.close()
-	clients = {}
-	for clientString in clientStrings:
-		clientStringPart = clientString.split(",")
-		client = new Node(clientStringPart[0],clientStringPart[1],clientStringPart[2])
-		clients [client.Id] = client
+    fp = open(file_name,"r")
+    #line in the form, clientID, IP and Port
+    clientStrings = fp.readlines()
+    fp.close()
+    clients = {}
+    for clientString in clientStrings:
+        clientStringPart = clientString.split(",")
+        client = Node(clientStringPart[0],clientStringPart[1],clientStringPart[2])
+        clients [client.Id] = client
 
-	return clients
+    return clients
 
 #Save clients' connection data to clients file
 def save_clients_file(file_name,clients):
-	fp = open(file_name,"w")
+    fp = open(file_name,"w")
 
-	for key in clients.keys():
-		clientObject = clients[key]
-		clientString = clientObject.Id+","+clientObject.IP+","+clientObject.Port+"\n"
-		fp.write(clientString)
-	fp.close()
+    for key in clients.keys():
+        clientObject = clients[key]
+        clientString = clientObject.Id+","+clientObject.IP+","+clientObject.Port+"\n"
+        fp.write(clientString)
+    fp.close()
 
 #not used
 #Add new client connection data to clients file
 def add_new_client(file_name, client):
-	fp = open(file_name,"a")
-	clientString = clientObject.Id+","+clientObject.IP+","+clientObject.Port+"\n"
-	fp.write(clientString)
-	fp.close()
+    fp = open(file_name,"a")
+    clientString = clientObject.Id+","+clientObject.IP+","+clientObject.Port+"\n"
+    fp.write(clientString)
+    fp.close()
 
 def broad_cast(message):
-	for key in client_dictionary.keys():
-		client = client_dictionary[key]
-		s = socket.socket()         # Create a socket object
-		host = socket.gethostbyaddr(client.IP)[0]
-		s.connect((host, server_port))
-		s.send(message)
-		s.close() 
+    for key in client_dictionary.keys():
+        client = client_dictionary[key]
+        s = socket.socket()         # Create a socket object
+        host = socket.gethostbyaddr(client.IP)[0]
+        s.connect((host, server_port))
+        s.send(message)
+        s.close() 
 
 
 
 def main ():
-	#Open a socket 
-	bind_socket()
-	#accept client messages
-
-
-
-
-
+    print "Starting RamseyCoin Server..."
+    #Open a socket 
+    sock = bind_socket(server_hostname, server_port)
+    print "listening on %s:%d." % (server_hostname, server_port)
+    #accept client messages
+    accept_connections(sock)
 
 #start server process
 main()
